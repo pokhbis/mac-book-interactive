9222cf23ff037983d9f04a7e5bc751b32f88c9e9
pokhbis <prokash748@gmail.com>
2021-08-21 22:22:50 +0600

    revised commit

--- js/index.js
+++ js/index.js
@@ -11,10 +11,18 @@ function totalCostCalculation() {
     totalCost.innerText = memoryCostValue + storageTotalValue + deliveryTotalValue + bestTotalValue;
     return totalCost.innerText;
 }
-
+//grand total Calculation
+function grandTotal() {
+    const grandTotal = document.getElementById('grand-total');
+    const totalCost = document.getElementById('total-price');
+    let totalCostValue = Number(totalCost.innerText);
+    grandTotal.innerText = totalCostValue;
+    return grandTotal.innerText;
+}
 //Memory section 
 function memorySection(memorySize, isFree) {
     document.getElementById(memorySize).addEventListener('click', function () {
+
         const memoryCost = document.getElementById('memory-cost');
         if (isFree == true) {
             memoryCost.innerText = '0';
@@ -25,6 +33,7 @@ function memorySection(memorySize, isFree) {
             memoryCostValue = Number(memoryCost.innerText);
         }
         totalCostCalculation();
+        grandTotal();
     })
 }
 memorySection('memory-8gb', true);
@@ -45,6 +54,7 @@ function storageSection(storageCapacity, isCostly) {
             storageCostValue = Number(storageCost.innerText);
         }
         totalCostCalculation();
+        grandTotal();
     })
 }
 storageSection('basic-ssd',);
@@ -54,6 +64,7 @@ storageSection('premier-ssd', true);
 //Delivery Charge Section
 function deliveryCost(deliveryType, withCost) {
     document.getElementById(deliveryType).addEventListener('click', function () {
+
         const deliveryCharge = document.getElementById('deliveryCharge');
         if (withCost == true) {
             deliveryCharge.innerText = '20';
@@ -64,6 +75,7 @@ function deliveryCost(deliveryType, withCost) {
             deliveryChargeValue = Number(deliveryCharge.innerText);
         }
         totalCostCalculation();
+        grandTotal();
     })
 }
 deliveryCost('express-delivery', true);
@@ -72,11 +84,15 @@ deliveryCost('free-delivery', false);
 //Apply Promo Code & Set Grand Total
 document.getElementById('promoCode-btn').addEventListener('click', function () {
     const grandTotal = document.getElementById('grand-total');
+    const totalCost = document.getElementById('total-price');
+    let totalCostValue = Number(totalCost.innerText);
+    grandTotal.innerText = totalCostValue;
     const promoCode = document.getElementById('apply-promoCode');
     let promoCodeText = promoCode.value;
     if (promoCodeText == 'stevekaku') {
         grandTotal.innerText = parseFloat('0.8' * totalCostCalculation());
     }
     promoCode.value = '';
+
 })
 
